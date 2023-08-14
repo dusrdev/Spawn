aae7acf402baa4318ff0767963a68a6b6d98c512
David Shnayder <dusrdev@outlook.com>
2023-08-14 16:48:36 +0300

    Check readme for details v1.2

--- README.md
+++ README.md
@@ -20,3 +20,23 @@ It works with the latest updates
 ## v1.1.1
 
 * Fixed bug where outputting text would cause an exception if quantity parameter is not provided. Now it will properly default to 1 if quantity is omitted
+
+## v1.2
+
+### Added Remove Function
+
+Spawned an item that can't be used, moved or destroyed? you are no longer stuck with it and forced to create a new save or change location.
+
+**spawn [id] rm [distance]** - command will remove item from the game, the distance can be omitted and will default to 5 (which is visually similar to 3-5 meters/yards)
+
+#### Debug Option
+
+**spawn [id] rm debug** - command will export information to a file on the desktop called "rmDebug.txt", this file will contain the requested item id, the player position, and all the found items positions and distance relative to the player.
+
+* As it is made for debugging, executing this command will not actually remove any of the items.
+
+### Added Second Debug Output Location
+
+**spawn log** - will activate secondary log location, in addition to the debug console, everything will be logged to a file name "SpawnLog.txt" on the desktop.
+
+* This is essentially a switch, you call it once, it will activate, call again to deactivate and so on. While it is on, it will continue logging to the file on the desktop until turned off / mod unloaded / game exited
--- Spawn/Spawn.cs
+++ Spawn/Spawn.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Collections.Generic;
+using System.Text;
 using System;
 using System.IO;
 using UnityEngine;
@@ -10,25 +12,26 @@ public class Spawn : Mod
 		Debug.Log("Mod Spawn has been loaded!");
 	}
 
+	private static readonly string DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
 	// Exports the item ids to a text file on the desktop
 	private static void ExportItemIds()
 	{
 		try
 		{
-			var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-			var path = Path.Combine(desktopPath, "itemIds.txt");
+			var path = Path.Combine(DesktopPath, "itemIds.txt");
 			var itemIds = Enum.GetNames(typeof(Enums.ItemID));
 			File.WriteAllLines(path, itemIds);
-			Debug.Log("ItemIds exported to: " + path);
+			Log("ItemIds exported to: " + path);
 		}
 		catch (Exception e)
 		{
-			Debug.Log("Error while exporting item ids: " + e.Message);
+			Log("Error while exporting item ids: " + e.Message);
 		}
 	}
 
 
-	[ConsoleCommand("spawn", "spawn [ItemID enum name or value] [Quantity, default=1]")]
+	[ConsoleCommand("spawn", "spawn [ID/help/log] [Quantity/rm] [MaxDistance/debug]")]
 	public static void Command(string[] args)
 	{
 		if (args.Length == 0)
@@ -36,58 +39,167 @@ public class Spawn : Mod
 			return;
 		}
 
-		var itemIdString = args[0];
-
-		if (itemIdString == "help")
+		if (args[0] == "help")
 		{
 			ExportItemIds();
 			return;
 		}
 
-		var itemDoesNotExist = ItemsManager.Get().StringToItemID(itemIdString) == -1;
-		if (itemDoesNotExist)
-		{
-			Debug.Log("ItemID does not exist: " + itemIdString);
+		if (args[0] == "log") {
+			_logToDesktop = !_logToDesktop;
 			return;
 		}
 
+		if (ItemsManager.Get().StringToItemID(args[0]) == -1)
+		{
+			Log("ItemID does not exist: " + args[0]);
+			return;
+		}
+
+		var segment = new ArraySegment<string>(args);
+		if (segment.Count > 1 && args[1] == "rm")
+		{
+			RemoveItem(segment);
+		}
+		else
+		{
+			SpawnItem(segment);
+		}
+	}
+
+	private static void SpawnItem(ArraySegment<string> args)
+	{
 		var quantity = 1;
 
-		if (args.Length > 1 && !int.TryParse(args[1], out quantity))
+		if (args.Count > 1 && !int.TryParse(args[1], out quantity))
 		{
-			Debug.Log("Invalid amount: " + args[1]);
+			Log("Invalid amount: " + args[1]);
 			return;
 		}
 
-		Item item = ItemsManager.Get().CreateItem(itemIdString, false);
-
-		if (item == null)
-		{
-			Debug.Log("ItemID does not exist: " + itemIdString);
-		}
+		var item = ItemsManager.Get().CreateItem(args[0], false);
 
 		if (item.m_Info.m_CanBeAddedToInventory)
 		{
-			Debug.Log("Spawning \"" + itemIdString + "\" in inventory");
+			Log("Spawning \"" + args[0] + "\" in inventory");
 			for (var i = 0; i < quantity; i++)
 			{
-				if (item == null) {
-					item = ItemsManager.Get().CreateItem(itemIdString, false);
+				if (item == null)
+				{
+					item = ItemsManager.Get().CreateItem(args[0], false);
 				}
 				InventoryBackpack.Get().InsertItem(item, null, null, true, true, true, true, true);
 				item = null;
 			}
 			return;
 		}
-		Debug.Log("Spawning at player: " + itemIdString);
+
+		Log("Spawning at player: " + args[0]);
 		var playerTransform = Player.Get().transform;
 		item.transform.position = playerTransform.position;
 		item.transform.rotation = playerTransform.rotation;
 		item.ItemsManagerRegister(true);
 	}
 
+	public static void RemoveItem(ArraySegment<string> args)
+	{
+		Enums.ItemID enumItemId;
+		Enum.TryParse(args[0], true, out enumItemId);
+
+		float maxDistance = 5f;
+		bool debug = false;
+
+		if (args.Count > 2)
+		{
+			if (args[2] == "debug")
+			{
+				debug = true;
+			}
+			else {
+				var isValid = float.TryParse(args[2], out maxDistance);
+				if (!isValid) {
+					Log("Invalid distance: " + args[2]);
+					return;
+				}
+			}
+		}
+
+		var playerTransform = Player.Get().transform;
+		var playerPosition = playerTransform.position;
+
+		var sb = new StringBuilder();
+		sb.Append("ItemID: ")
+		  .AppendLine(enumItemId.ToString())
+		  .Append("MaxDistance: ")
+		  .AppendLine(maxDistance.ToString())
+		  .Append("Debug: ")
+		  .AppendLine(debug.ToString())
+		  .AppendLine()
+		  .Append("PlayerPosition: ")
+		  .AppendLine(playerPosition.ToString())
+		  .AppendLine();
+
+		List<Item> items = new List<Item>();
+		foreach (var item in Item.s_AllItems)
+		{
+			if (item.m_Info.m_ID != enumItemId)
+			{
+				continue;
+			}
+
+			var itemPosition = item.transform.position;
+			var distance = Vector3.Distance(playerPosition, itemPosition);
+
+			if (debug)
+			{
+				sb.Append("ItemPosition: ")
+				  .AppendLine(itemPosition.ToString())
+				  .Append("Distance: ")
+				  .AppendLine(distance.ToString());
+			}
+
+			if (distance > maxDistance)
+			{
+				continue;
+			}
+			items.Add(item);
+		}
+
+		if (debug)
+		{
+			var path = Path.Combine(DesktopPath, "rmDebug.txt");
+			File.WriteAllText(path, sb.ToString());
+		}
+
+		while (!debug && items.Count > 0)
+		{
+			var item = items[0];
+			items.RemoveAt(0);
+			item.m_Info.m_CanBeRemovedFromInventory = true;
+			item.m_Info.m_DestroyByItemsManager = true;
+			item.m_Info.m_CantDestroy = false;
+			ItemsManager.Get().AddItemToDestroy(item);
+		}
+	}
+
+	private static readonly string _logPath = Path.Combine(DesktopPath, "SpawnLog.txt");
+
+	private static bool _logToDesktop = false;
+
+	public static void Log(string message)
+	{
+		Debug.Log(message);
+		if (!_logToDesktop)
+		{
+			return;
+		}
+		var time = DateTime.Now.ToString();
+		var output = string.Format("[{0}] {1}{2}", time, message, Environment.NewLine);
+		File.AppendAllText(_logPath, output);
+	}
+
 	public void OnModUnload()
 	{
-		Debug.Log("Mod Spawn has been unloaded!");
+		Log("Mod Spawn has been unloaded!");
 	}
 }
